beb9321fde353126dc1b75bde5f3c7d0018115d7
anton <mrsuh6@gmail.com>
2017-10-30 21:02:46 +0300

    message view

--- src/model/api.go
+++ src/model/api.go
@@ -11,29 +11,33 @@ func FormatMessage(db *dbal.DBAL, note dbal.Note) string {
 
 	var b bytes.Buffer
 
+	b.WriteString("<b>")
 	b.WriteString(formatType(note.Type))
-	b.WriteString(" за ")
-	b.WriteString(formatPrice(note.Price))
-	b.WriteString(" руб/мес")
+	price := formatPrice(note.Price)
+	if price != "" {
+		b.WriteString(" за ")
+		b.WriteString(price)
+		b.WriteString(" руб/мес")
+	}
 
 	text_subways := formatSubways(db, note)
 	if text_subways != "" {
 		b.WriteString(" около метро ")
 		b.WriteString(text_subways)
 	}
+	b.WriteString("</b>")
+	b.WriteString("\n")
+
+	b.WriteString(fmt.Sprintf("%s <a href='%s'>Перейти к объявлению</a>", note.Contact, note.Link))
 
 	b.WriteString("\n")
+
 	b.WriteString(note.Description)
 
 	if len(note.Photos) != 0 {
 		b.WriteString("\n")
-		b.WriteString(strings.Join(note.Photos, "\n"))
+		b.WriteString(note.Photos[0])
 	}
-
-	b.WriteString("\n")
-	b.WriteString(note.Contact)
-	b.WriteString("\n")
-	b.WriteString(note.Link)
 
 	return b.String()
 }
@@ -41,7 +45,7 @@ func FormatMessage(db *dbal.DBAL, note dbal.Note) string {
 func formatType(note_type int) string {
 	type_string := "";
 	if note_type == 0 {
-		type_string = "комната";
+		type_string = "Комната";
 	} else if note_type == 1 {
 		type_string = "1 комнатная квартира";
 	} else if note_type == 2 {
@@ -51,7 +55,7 @@ func formatType(note_type int) string {
 	} else if note_type == 4 {
 		type_string = "4+ комнатная квартира";
 	} else if note_type == 5 {
-		type_string = "студия";
+		type_string = "Студия";
 	}
 
 	return type_string;
--- src/model/telegram.go
+++ src/model/telegram.go
@@ -19,7 +19,7 @@ func (telegram *Telegram) SendMessage(messages chan Message) {
 
 	for message := range messages {
 
-		body := fmt.Sprintf(`{"chat_id": %d, "text": "%s"}`, message.ChatId, message.Text)
+		body := fmt.Sprintf(`{"chat_id": %d, "text": "%s", "parse_mode": "HTML"}`, message.ChatId, message.Text)
 
 		http.Post(fmt.Sprintf("https://api.telegram.org/bot%s/%s", telegram.Token, "sendMessage"), "application/json", bytes.NewBuffer([]byte(body)))
 
