19059443fd5377297f2aa38e0b877930d3544188
anton <mrsuh6@gmail.com>
2017-11-21 22:46:17 +0300

    vk remove blocked recipients

--- src/model/telegram.go
+++ src/model/telegram.go
@@ -12,18 +12,20 @@ type Telegram struct {
 	Token string
 }
 
+const TELEGRAM_URL = "https://api.telegram.org/bot%s/sendMessage"
+
 func (telegram *Telegram) SendMessage(messages chan Message) {
 
 	for message := range messages {
 
 		body := fmt.Sprintf(`{"chat_id": %d, "text": "%s", "parse_mode": "HTML"}`, message.ChatId, message.Text)
 
-		resp, err := http.Post(fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", telegram.Token), "application/json", bytes.NewBuffer([]byte(body)))
+		resp, err := http.Post(fmt.Sprintf(TELEGRAM_URL, telegram.Token), "application/json", bytes.NewBuffer([]byte(body)))
 
 		defer resp.Body.Close()
 
 		if nil != err {
-			log.Printf("request err: %s", err)
+			log.Printf("request err {chatId: %v, err: %s}", message.ChatId, err)
 		}
 
 		time.Sleep(35 * time.Millisecond) //30 rps
